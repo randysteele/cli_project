4d32fec5a5962fcae716143a5217c830fce175e5
Randy Steele <steelerx155@gmail.com>
2019-11-22 16:17:37 +0000

    updated cli class

--- lib/cli_project.rb
+++ lib/cli_project.rb
@@ -1,3 +1,5 @@
-require 'pry'
-require_relative "cli_project/version"
+require_relative 'cli_project/version'
+require_relative 'cli_project/api'
+require_relative 'cli_project/cli'
+require_relative 'cli_project/music'
 
---
+++ lib/cli_project/cli.rb
@@ -0,0 +1,11 @@
+class CLI
+  
+  
+  def call
+     list_songs
+  end
+  
+  def list_songs
+   "Today's 50 most popular songs are:" 
+ end
+end
