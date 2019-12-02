04117d32ad94a89cd573b837825ac19a21a05e45
Randy Steele <steelerx155@gmail.com>
2019-12-02 00:30:01 +0000

    updated api class

--- lib/cli_project/api.rb
+++ lib/cli_project/api.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class API 
-  # attr_accessor :CLI, :artists, :album, :tracks, :name
+  
   
   RSpotify.authenticate("f2d6e54d3bed499983183cb8c183dd75", "14e98f258cce4f89b5eb48f8342f6f37")
   
@@ -9,28 +9,21 @@ class API
     until input == "exit"
     puts "Welcome to today's 50 most popular songs!"
     puts "To list all the popular songs, enter 'top 50'."
-    puts "Would you like to see the artists for these songs?"
-    input = gets.strip.to_i
-    
-    if input = "y"
-      top_50_artists
-    end
-    
     puts "To exit, type 'exit'."
-    puts "What would you like to do?"
     
     input = gets.strip
     
-    artist = playlist.tracks.find(input.to_i)
-    exiend
-    case input 
-     when "top 50"
+   if input == "top 50"
       top_50
-      # when #{i}
-      #   song_number{i}
+      puts "Would you like to see the artists for these songs?"
+      elsif input == "y"
+        top_50_artists
+        elsif input == "n" 
+        input == ""
     end
+    
   end
-end
+ end
 
    def playlist
      playlist = RSpotify::Playlist.find('jppromotions', '35pMFxG8cjdgqevqCOVPAr')
@@ -39,7 +32,7 @@ end
     
    def top_50  
      playlist.tracks.map.with_index(1) do |song, i|
-     "#{i}. #{song.name}"
+     puts "#{i}. #{song.name}"
    end
   end
   
@@ -47,21 +40,21 @@ end
     playlist.tracks.map.with_index(1) do |art, i|
     puts "#{i}. #{art.artists[0].name}"
     
-  end
+    end
   end
  
  
-  def print_artists(from_number)
-    puts "Artists #{from_number} - #{from_number+49}"
+  # def print_artists(from_number)
+  #   puts "Artists #{from_number} - #{from_number+49}"
 
-    playlist.tracks[from_number-1, 50].map.with_index(from_number) do |art, i|
-    puts "#{i}. #{art.name}"
-   end
+  #   playlist.tracks[from_number-1, 50].map.with_index(from_number) do |art, i|
+  #   puts "#{i}. #{art.name}"
+  # end
    
-   def song_number
-     playlist.tracks.map.with_index do |song, i|
-       puts "#{i}. #{song.name}" 
-     end
-   end
- end
-  end
+  # def song_number
+  #   playlist.tracks.map.with_index do |song, i|
+  #     puts "#{i}. #{song.name}" 
+  #   end
+  # end
+# end
+end
