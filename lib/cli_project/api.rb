796879f4b9fbe2008f1655a0399401112af316fd
Randy Steele <steelerx155@gmail.com>
2019-12-03 16:03:09 +0000

    updated api

--- lib/cli_project/api.rb
+++ lib/cli_project/api.rb
@@ -2,28 +2,39 @@ require 'pry'
 class API 
   
 
-  def list_artists
+  # def list_artists
+  #   playlist.tracks.sort{ |a, b| a.name <=> b.name }.map.with_index(1)  do |s, i|
+  #   puts "#{i}. #{s.artists[0].name}"
+  #   end
+  # end  
+  
+  def list_songs
     playlist.tracks.sort{ |a, b| a.name <=> b.name }.map.with_index(1)  do |s, i|
-    puts "#{i}. #{s.artists[0].name}"
-    
+    puts "#{i}. #{s.name}"
+    end
+  end  
   
   def call
     input = ""
     until input == "exit"
     puts "To list all the popular songs, enter 'top 50'."
     puts "To exit, type 'exit'."
-    
     puts "What song number would you like too see?"
-    input = gets.strip
+    input = gets.strip.downcase
     
+   if input.to_i > 0
+     playlist.tracks.map.with_index(1) do |art, i|
+    puts "#{i}. #{art.artists[0].name}"
+  end 
+     
+     == "top 50"
+     list_songs
+    elsif 
     
-    
-    # if input.to_i > 0
-    #   puts "#{i}. #{s.artists[0].name}"
+   end
     
   end
     
-  end
     def print_songs(number)  
       playlist.tracks[number-1, 50].map.with_index(number) do |song, index|
         puts "#{index}. #{song.name}"
@@ -40,6 +51,7 @@ class API
   #   end
    end
   end
+  
  end
 
   def playlist
